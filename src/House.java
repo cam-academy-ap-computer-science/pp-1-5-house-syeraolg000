ff8054fe6b451dd8aad9f1803127d9d8e3a0066a
syeraolg000 <syeraolg000@CAM-LAB-28.bgsd.k12.wa.us>
2019-09-23 07:39:44 -0700

    House

--- src/House.java
+++ src/House.java
@@ -3,6 +3,43 @@ public class House {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("This is the house that Jack built.");
+		System.out.println();
+		System.out.println("This is the malt");
+		System.out.println("That lay in the house that Jack built.");
+		System.out.println();
+		System.out.println("This is the rat,");
+		System.out.println("That ate the malt");
+		System.out.println("That lay in the house that Jack built.");
+		System.out.println();
+		System.out.println("This is the cat,");
+		System.out.println("That killed the rat,");
+		System.out.println("That ate the malt");
+		System.out.println("That lay in the house that Jack built.");
+		System.out.println();
+		System.out.println("This is the dog,");
+		System.out.println("That worried the cat,");
+		System.out.println("That killed the rat,");
+		System.out.println("That ate the malt");
+		System.out.println("That lay in the house that Jack built.");
+		System.out.println();
+		System.out.println("This is the cow with the crumpled horn,");
+		System.out.println("That tossed the dog,");
+		System.out.println("That worried the cat,");
+		System.out.println("That killed the rat,");
+		System.out.println("That ate the malt");
+		System.out.println("That lay in the house that Jack built.");
+		System.out.println();
+		System.out.println("This is the maiden all forlorn");
+		System.out.println("That milked the cow with the crumpled horn,");
+		System.out.println("That tossed the dog,");
+		System.out.println("That worried the cat,");
+		System.out.println("That killed the rat,");
+		System.out.println("That ate the malt");
+		System.out.println("That lay in the house that Jack built.");
+	
+		
+
 
 	}
 
