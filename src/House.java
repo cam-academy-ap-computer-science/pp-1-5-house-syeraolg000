bdaa8f1bbd04f7e1c91ec8dd9a07d028ebcfcf45
syeraolg000 <syeraolg000@CAM-LAB-28.bgsd.k12.wa.us>
2019-09-26 06:57:14 -0700

    finshed with this project

--- src/House.java
+++ src/House.java
@@ -6,41 +6,44 @@ public class House {
 		System.out.println("This is the house that Jack built.");
 		System.out.println();
 		System.out.println("This is the malt");
-		System.out.println("That lay in the house that Jack built.");
+		House();
 		System.out.println();
 		System.out.println("This is the rat,");
-		System.out.println("That ate the malt");
-		System.out.println("That lay in the house that Jack built.");
+		malt();
 		System.out.println();
 		System.out.println("This is the cat,");
-		System.out.println("That killed the rat,");
-		System.out.println("That ate the malt");
-		System.out.println("That lay in the house that Jack built.");
+		rat();
 		System.out.println();
 		System.out.println("This is the dog,");
-		System.out.println("That worried the cat,");
-		System.out.println("That killed the rat,");
-		System.out.println("That ate the malt");
-		System.out.println("That lay in the house that Jack built.");
+		cat();
 		System.out.println();
 		System.out.println("This is the cow with the crumpled horn,");
-		System.out.println("That tossed the dog,");
-		System.out.println("That worried the cat,");
-		System.out.println("That killed the rat,");
-		System.out.println("That ate the malt");
-		System.out.println("That lay in the house that Jack built.");
+		dog();
 		System.out.println();
 		System.out.println("This is the maiden all forlorn");
 		System.out.println("That milked the cow with the crumpled horn,");
-		System.out.println("That tossed the dog,");
-		System.out.println("That worried the cat,");
-		System.out.println("That killed the rat,");
-		System.out.println("That ate the malt");
-		System.out.println("That lay in the house that Jack built.");
-	
-		
-
-
+		dog();
 	}
+	public static void House() {
+		System.out.println("That lay in the house that Jack built.");
+	} 
+	public static void malt() {
+		System.out.println("That ate the malt,");
+		House();
+	} 
+	public static void rat() {
+		System.out.println("That killed the rat,");
+		malt();
+	}
+	public static void cat() {
+		System.out.println("That worried the cat,");
+		rat();
+	}
+	public static void dog() {
+		System.out.println("That tossed the dog,");
+		cat(); 
+	} 
+
 
 }
+
